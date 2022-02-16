a2d32fb9863068e4cb8ac70ff4ad69aa8984ba27
cuongtech47 <cuongtech47@gmail.com>
2022-02-16 11:04:07 +0700

    upload node

--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-var nodemailer = require('nodemailer');
+var mysql = require('mysql');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'cuongtech47@gmail.com',
-    pass: 'cuongpro82'
-  }
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: ""
 });
 
-var mailOptions = {
-  from: 'tncuong.19it4@vku.udn.vn',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
 });
